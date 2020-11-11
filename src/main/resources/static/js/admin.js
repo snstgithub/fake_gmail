b0783721caeaa2dfdbf31e5bd68df9d2b5f86871
kali <primat.2000@mail.ru>
2020-11-11 06:40:48 +0500

    yangilandi

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -2,5 +2,18 @@ function all() {
     axios.get("/admin/api/all")
         .then(function (response) {
             console.log(response)
+            let all=JSON.parse(JSON.stringify(response.data))
+            let out="";
+
+            for (let i = 0; i <all.length; i++) {
+                out+="<div class=\"col-5 border\">\n" +
+                    "        "+all[i].email+"" +
+                    "    </div>\n" +
+                    "\n" +
+                    "    <div class=\"col-5 border\">\n" +
+                    "        "+all[i].password+"" +
+                    "    </div>";
+            }
+            document.getElementById('root').innerHTML=out;
         })
 }
